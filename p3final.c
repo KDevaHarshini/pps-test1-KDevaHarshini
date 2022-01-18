79b5380dc4bbb3060aaf7fd643ff33bb472b36e5
kancharla devaharshini <94678326+KDevaHarshini@users.noreply.github.com>
2022-01-18 10:36:53 +0000

    p3 revised

--- p3final.c
+++ p3final.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
-void input(int*a,int*b)
+void input(int *a,int *b)
 {
   printf("Enter two number");
   scanf("%d %d",a,b);
 }
-void add(int a,int b,int*sum)
+void add(int a,int b,int *sum)
 {
-  *sum = a+b;
+  *sum=a+b;
 }
 void output(int a,int b,int sum)
 {
-  printf("sum of %d + %d is %d\n",a,b,sum);
+  printf("sum of %d +%d is %d\n",a,b,sum);
 }
 int main()
 {
