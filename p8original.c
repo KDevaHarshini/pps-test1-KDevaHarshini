382c57f6dfaa3180b2107e397bd2d6c75d5e4ea1
kancharla devaharshini <94678326+KDevaHarshini@users.noreply.github.com>
2022-01-24 06:53:13 +0000

    p8 revised

--- p8original.c
+++ p8original.c
@@ -11,22 +11,22 @@ float squareroot(float n)
   float x = n;
   float y = 1;
   float e = 0.000001;
-  while (x - y > e)
+  while(x - y > e)
 {
   x = (x+y)/2;
   y = n/x;
 }
 return x;
 }
-  void output(float n,float sres)
-  {
-    printf("Square root of given input %0.2f is = %0.2f\n",n,sres);
-  }
+void output(float n,float sres)
+{
+  printf("Square root of given input %0.2f is = %0.2f\n",n,sres);
+}
 int main()
 {
   float s,s1;
-  s=input();
-  s1=squareroot(s);
+  s = input();
+  s1 = squareroot(s);
   output(s,s1);
   return 0;
 }
